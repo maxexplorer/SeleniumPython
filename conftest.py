1ba0bd2818ef5023a33bad4bfae7bf38717bb5fe
maxexplorer <maxexplorer@mail.ru>
2022-10-12 08:10:15 +0300

    amend files conftest.py and test_items.py

--- conftest.py
+++ conftest.py
@@ -2,16 +2,22 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
 
-@pytest.fixture()
-def browser():
-    print("\nstart browser for test..")
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose user language: en/ru/es.....(etc)")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    print("\nstart chrome browser for test..")
     browser = webdriver.Chrome(
         executable_path="C:/Users/Макс/PycharmProjects/SeleniumPython/chromedriver/chromedriver.exe",
-        options=options
-    )
+        options=options)
+
     yield browser
     print("\nquit browser..")
     browser.quit()
--- lesson4/test_items.py
+++ lesson4/test_items.py
@@ -5,5 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
 def test_button_find(browser):
     browser.get(link)
-    button = browser.find_elements(By.TAG_NAME, "button")
-    assert button is not None, 'Not found'
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    assert button.is_enabled, 'Not found'
