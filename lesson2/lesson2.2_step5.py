2073729de0097b9f7b5c1ecfa099993f3ba0e4fe
maxexplorer <maxexplorer@mail.ru>
2022-10-12 17:23:27 +0300

    add files base_page.py and main_page.py

--- lesson2/lesson2.2_step5.py
+++ lesson2/lesson2.2_step5.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-browser = webdriver.Chrome(executable_path="/chromedriver/chromedriver.exe")
+browser = webdriver.Chrome(executable_path="C:/Users/Макс/PycharmProjects/SeleniumPython/chromedriver/chromedriver.exe")
 link = "https://SunInJuly.github.io/execute_script.html"
 browser.get(link)
 # button = browser.find_element(By.TAG_NAME, "button")
