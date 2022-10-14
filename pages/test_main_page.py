32896ec493d7e0cf13758d32482ac954912ed4cf
maxexplorer <maxexplorer@mail.ru>
2022-10-14 19:08:18 +0300

    add class_MainPages and file test_main_page.py

--- pages/test_main_page.py
+++ pages/test_main_page.py
@@ -1,7 +1,10 @@
+from pages.main_page import MainPage
 from selenium.webdriver.common.by import By
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    page = MainPage(browser, link)
+    page.open()
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
